8f700de8174fb1873e367204ef38fbc4abe50271
JnanadaBhat <janu28.bhat@gmail.com>
2020-11-18 06:58:50 -0500

    Added auth

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,6 +13,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+Amplify.configure(config);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
